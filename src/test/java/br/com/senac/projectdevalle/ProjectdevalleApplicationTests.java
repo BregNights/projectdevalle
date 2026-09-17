d04484b3ce89d28c5daf16799ff4326ddb20f3b9
Daniel <daniel_baumann567@hotmail.com>
2026-09-17 19:14:00 -0300

    Update ProjectdevalleApplicationTests.java

--- src/test/java/br/com/senac/projectdevalle/ProjectdevalleApplicationTests.java
+++ src/test/java/br/com/senac/projectdevalle/ProjectdevalleApplicationTests.java
@@ -1,10 +1,9 @@
 package br.com.senac.projectdevalle;
 
+import br.com.senac.projectdevalle.support.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ProjectdevalleApplicationTests {
+class ProjectdevalleApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
